3588880cb86fa630b9f679465fda927f178c8465
Wira Mahendra <wiramahendra@proton.me>
2025-12-06 21:24:09 +0800

    Docs

--- web/apps/web-docs/mdx-components.tsx
+++ web/apps/web-docs/mdx-components.tsx
@@ -1,9 +1,18 @@
 import { Tab, Tabs } from '@/components/Tabs';
+import { CopyPageButton } from '@/components/CopyPageButton';
 
 export function useMDXComponents(components: any): any {
   return {
     Tab,
     Tabs,
+    h1: (props: any) => (
+      <div className="relative">
+        <h1 {...props} />
+        <div className="absolute top-0 right-0">
+          <CopyPageButton />
+        </div>
+      </div>
+    ),
     ...components,
   };
 }
