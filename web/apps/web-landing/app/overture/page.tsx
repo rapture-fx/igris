6ad1c0e21012d995d26546293332d53a6838dd12
Wira Mahendra <wiramahendra@proton.me>
2026-01-07 12:24:23 +0800

    clean

--- web/apps/web-landing/app/overture/page.tsx
+++ web/apps/web-landing/app/overture/page.tsx
@@ -80,15 +80,6 @@ export default function OverturePage() {
                 borderLeft: '0.5px solid rgba(156, 163, 175, 0.3)',
                 borderRight: '0.5px solid rgba(156, 163, 175, 0.3)'
               }}>
-                <div className="absolute -top-4 -left-4 w-8 h-8">
-                  <div className="absolute top-3.5 left-0 w-8 border-t-[0.5px] border-[#1a1e21]"></div>
-                  <div className="absolute top-0 left-3.5 h-8 border-l-[0.5px] border-[#1a1e21]"></div>
-                </div>
-                <div className="absolute -bottom-4 -right-4 w-8 h-8">
-                  <div className="absolute bottom-3.5 right-0 w-8 border-b-[0.5px] border-[#1a1e21]"></div>
-                  <div className="absolute bottom-0 right-3.5 h-8 border-r-[0.5px] border-[#1a1e21]"></div>
-                </div>
-
                 <div className="max-w-[1100px] mx-auto pt-24 px-0 md:px-8 lg:px-16 relative z-10">
                   <div className="pt-24 mb-6">
                     <button
@@ -129,15 +120,6 @@ export default function OverturePage() {
                 backgroundColor: '#f6f6f4',
                 minHeight: '600px'
               }}>
-                <div className="absolute -top-4 -left-4 w-8 h-8">
-                  <div className="absolute top-3.5 left-0 w-8" style={{ borderTop: '0.3px solid #1a1e21' }}></div>
-                  <div className="absolute top-0 left-3.5 h-8" style={{ borderLeft: '0.3px solid #1a1e21' }}></div>
-                </div>
-                <div className="absolute -bottom-4 -right-4 w-8 h-8">
-                  <div className="absolute bottom-3.5 right-0 w-8" style={{ borderBottom: '0.3px solid #1a1e21' }}></div>
-                  <div className="absolute bottom-0 right-3.5 h-8" style={{ borderRight: '0.3px solid #1a1e21' }}></div>
-                </div>
-
                 <div className="w-full px-0">
                   {/* Vertical Divider positioned at center - full height from top to bottom of frame */}
                   <div className="absolute top-0 bottom-0 left-2/3 hidden lg:block" style={{
@@ -200,15 +182,6 @@ export default function OverturePage() {
                 backgroundColor: '#f6f6f4',
                 minHeight: '600px'
               }}>
-                <div className="absolute -top-4 -left-4 w-8 h-8">
-                  <div className="absolute top-3.5 left-0 w-8" style={{ borderTop: '0.3px solid #1a1e21' }}></div>
-                  <div className="absolute top-0 left-3.5 h-8" style={{ borderLeft: '0.3px solid #1a1e21' }}></div>
-                </div>
-                <div className="absolute -bottom-4 -right-4 w-8 h-8">
-                  <div className="absolute bottom-3.5 right-0 w-8" style={{ borderBottom: '0.3px solid #1a1e21' }}></div>
-                  <div className="absolute bottom-0 right-3.5 h-8" style={{ borderRight: '0.3px solid #1a1e21' }}></div>
-                </div>
-
                 <div className="w-full px-0">
                   {/* Vertical Divider positioned at center - full height from top to bottom of frame */}
                   <div className="absolute top-0 bottom-0 left-2/3 hidden lg:block" style={{
@@ -271,21 +244,11 @@ export default function OverturePage() {
             <div className="mx-auto max-w-[1200px] px-4 sm:px-6 lg:px-8">
               <div className="relative py-12 md:py-16 lg:py-20 px-4 md:px-8 lg:px-12" style={{
                 borderTop: '0.5px solid rgba(156, 163, 175, 0.3)',
-                borderBottom: '0.5px solid rgba(156, 163, 175, 0.3)',
                 borderLeft: '0.5px solid rgba(156, 163, 175, 0.3)',
                 borderRight: '0.5px solid rgba(156, 163, 175, 0.3)',
                 backgroundColor: '#f6f6f4',
                 minHeight: '600px'
               }}>
-                <div className="absolute -top-4 -left-4 w-8 h-8">
-                  <div className="absolute top-3.5 left-0 w-8" style={{ borderTop: '0.3px solid #1a1e21' }}></div>
-                  <div className="absolute top-0 left-3.5 h-8" style={{ borderLeft: '0.3px solid #1a1e21' }}></div>
-                </div>
-                <div className="absolute -bottom-4 -right-4 w-8 h-8">
-                  <div className="absolute bottom-3.5 right-0 w-8" style={{ borderBottom: '0.3px solid #1a1e21' }}></div>
-                  <div className="absolute bottom-0 right-3.5 h-8" style={{ borderRight: '0.3px solid #1a1e21' }}></div>
-                </div>
-
                 <div className="w-full px-0">
                   {/* Vertical Divider positioned at center - full height from top to bottom of frame */}
                   <div className="absolute top-0 bottom-0 left-2/3 hidden lg:block" style={{
