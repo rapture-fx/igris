52d94236f4c35c933e73a2b5c78fbbd68056b363
Wira Mahendra <wiramahendra@proton.me>
2026-02-08 13:20:37 +0800

    updte

--- web/apps/web-landing/src/components/sections/WhyItExists.tsx
+++ web/apps/web-landing/src/components/sections/WhyItExists.tsx
@@ -6,7 +6,7 @@ export default function WhyItExists() {
   return (
     <section className="bg-[#f6f6f4] dark:bg-dark-bg text-gray-900 dark:text-[#f6f6f4] transition-colors duration-200">
       <div className="mx-auto max-w-[1300px] px-4 sm:px-6 lg:px-8">
-        <div className="relative px-4 md:px-8 lg:px-12 border-l border-r border-b border-gray-300 dark:border-[#f6f6f4]/10" style={{ height: '600px' }}>
+        <div className="relative px-4 md:px-8 lg:px-12 section-border" style={{ height: '600px' }}>
           <img
             src="/rock.png"
             alt=""
