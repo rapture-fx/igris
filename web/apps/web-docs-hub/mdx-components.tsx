352049bbdf5f870d8568075e2d80f007d30f4df9
Wira Mahendra <wiramahendra@proton.me>
2026-03-02 14:56:03 +0800

    mdx

--- web/apps/web-docs-hub/mdx-components.tsx
+++ web/apps/web-docs-hub/mdx-components.tsx
@@ -1,8 +1,12 @@
 import { CodeBlock } from '@/components/CodeBlock';
+import { DiagramTabs } from '@/components/DiagramTabs';
+import { StepChain } from '@/components/StepChain';
 
 export function useMDXComponents(components: any): any {
   return {
     pre: (props: any) => <CodeBlock {...props} />,
+    DiagramTabs,
+    StepChain,
     ...components,
   };
 }
