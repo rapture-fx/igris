69d9c3cc478ccd7f9cea059f5fbbbc875d655c75
Wira Mahendra <wiramahendra@proton.me>
2026-03-05 09:39:15 +0800

    cost

--- web/apps/web-console/components/layout/DashboardLayout.tsx
+++ web/apps/web-console/components/layout/DashboardLayout.tsx
@@ -64,7 +64,7 @@ export function DashboardLayout({ children }: DashboardLayoutProps) {
                 </div>
               )}
 
-              <div className="px-4 sm:px-6 lg:px-8 py-6 pb-16">
+              <div className="px-4 sm:px-6 lg:px-8 py-6 pb-16 max-w-6xl mx-auto">
                 {children}
               </div>
             </main>
