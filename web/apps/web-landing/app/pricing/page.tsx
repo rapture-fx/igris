ff4f892a0639dbee792ea93c1f4e71fb5f5e8209
Wira Mahendra <wiramahendra@proton.me>
2026-03-06 22:56:17 +0800

    ok

--- web/apps/web-landing/app/pricing/page.tsx
+++ web/apps/web-landing/app/pricing/page.tsx
@@ -1,34 +1,21 @@
-'use client';
-
 import Header from '../../src/components/sections/Header'
 import Footer from '../../src/components/sections/Footer'
 import Pricing from '../../src/components/sections/Pricing'
 import Faq from '../../src/components/sections/Faq'
 import ClosingPosition from '../../src/components/sections/ClosingPosition'
-import EarlyAccessModal from '../../src/components/modals/EarlyAccessModal'
-import { useModal } from '../../src/contexts/ModalContext'
 
 export default function PricingPage() {
-  const { isEarlyAccessModalOpen, closeEarlyAccessModal } = useModal();
-
   return (
-    <>
-      <div className="relative min-h-screen bg-white dark:bg-[#0A0A0A]">
-
-        <div className="relative z-10">
-          <Header />
-          <main>
-            <Pricing />
-            <Faq />
-            <ClosingPosition />
-          </main>
-          <Footer />
-        </div>
+    <div className="relative min-h-screen bg-white dark:bg-[#0A0A0A]">
+      <div className="relative z-10">
+        <Header />
+        <main>
+          <Pricing />
+          <Faq />
+          <ClosingPosition />
+        </main>
+        <Footer />
       </div>
-      <EarlyAccessModal
-        isOpen={isEarlyAccessModalOpen}
-        onClose={closeEarlyAccessModal}
-      />
-    </>
+    </div>
   );
 }
