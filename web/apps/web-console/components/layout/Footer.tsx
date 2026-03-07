60d61622cc976df1fb6c534e0e70654220583a8c
Wira Mahendra <wiramahendra@proton.me>
2026-03-07 14:28:58 +0800

    ok

--- web/apps/web-console/components/layout/Footer.tsx
+++ web/apps/web-console/components/layout/Footer.tsx
@@ -2,6 +2,6 @@
 
 export function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 md:left-56 right-0 z-40 h-1 bg-[#f3f3f6] dark:bg-[#25231e]" />
+    <footer className="fixed bottom-0 left-0 md:left-56 right-0 z-40 h-2 bg-[#f3f3f6] dark:bg-[#25231e]" />
   );
 }
