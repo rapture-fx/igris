bed69a0e6ab3398a1385762c4345ac9f4e7373ec
Wira Mahendra <wiramahendra@proton.me>
2026-03-07 14:02:25 +0800

    ok

--- web/apps/web-console/components/layout/Navbar.tsx
+++ web/apps/web-console/components/layout/Navbar.tsx
@@ -9,7 +9,7 @@ interface NavbarProps {
 
 export function Navbar({ onMenuClick }: NavbarProps) {
   return (
-    <nav className="fixed top-0 left-0 md:left-64 right-0 z-40 h-12 bg-background md:pl-12 md:pr-2">
+    <nav className="fixed top-0 left-0 md:left-56 right-0 z-40 h-2 bg-[#f3f3f6] dark:bg-[#25231e] md:pl-12 md:pr-2">
       <div className="h-full px-4 flex items-center md:justify-end">
         {/* Mobile menu trigger */}
         <Button
