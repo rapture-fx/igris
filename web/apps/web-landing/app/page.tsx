6ab28c6a0bfb8eb0848384dfc90767ed3e9cd7a6
Wira Mahendra <wiramahendra@proton.me>
2026-03-09 08:12:30 +0800

    ok

--- web/apps/web-landing/app/page.tsx
+++ web/apps/web-landing/app/page.tsx
@@ -35,7 +35,6 @@ export default function HomePage() {
           <Header />
           <main>
             <Hero />
-            <InstallSection />
             <Products />
             <CoreCapabilities />
             <AutonomousSystems />
