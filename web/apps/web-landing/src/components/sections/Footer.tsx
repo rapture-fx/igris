dc241a93eb1bda756e42cd24fb3fe351b01319dd
Wira Mahendra <wiramahendra@proton.me>
2026-03-14 10:27:40 +0800

    ok

--- web/apps/web-landing/src/components/sections/Footer.tsx
+++ web/apps/web-landing/src/components/sections/Footer.tsx
@@ -183,7 +183,7 @@ export default function Footer() {
                 <img
                   src={mounted && theme === 'dark' ? '/dmfoot.png' : '/foot.png'}
                   alt="Igris Inertial"
-                  className="h-8 w-auto"
+                  className="h-6 w-auto"
                 />
               </div>
             </div>
