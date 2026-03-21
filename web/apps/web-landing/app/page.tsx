879081c278a543b989e7f5f6e24fb182a7253c2e
Wira Mahendra <wiramahendra@proton.me>
2026-03-21 09:22:07 +0800

    llms

--- web/apps/web-landing/app/page.tsx
+++ web/apps/web-landing/app/page.tsx
@@ -15,39 +15,28 @@ import AutonomousSystems from '../src/components/sections/AutonomousSystems'
 
 import UseCasesPopup from '../src/components/popups/UseCasesPopup'
 
-import AIAgentView from '../src/components/AIAgentView'
-import ViewModeToggle from '../src/components/ViewModeToggle'
-
 import { useProductPopup } from '../src/contexts/ProductPopupContext'
-import { useViewMode } from '../src/contexts/ViewModeContext'
 
 export default function HomePage() {
 
   const { isUseCasesOpen, closeUseCases } = useProductPopup();
-  const { isAIAgentMode } = useViewMode();
 
   return (
     <>
-      {isAIAgentMode ? (
-        <AIAgentView />
-      ) : (
-        <div className="min-h-screen bg-white dark:bg-[#0A0A0A] transition-colors duration-200">
-          <Header />
-          <main>
-            <Hero />
-            <Products />
-            <CoreCapabilities />
-            <AutonomousSystems />
-            <SDKs />
-            <MultiTenancy />
-            <WhyItExists />
-            <ClosingPosition />
-          </main>
-          <Footer />
-        </div>
-      )}
-
-      <ViewModeToggle />
+      <div className="min-h-screen bg-white dark:bg-[#0A0A0A] transition-colors duration-200">
+        <Header />
+        <main>
+          <Hero />
+          <Products />
+          <CoreCapabilities />
+          <AutonomousSystems />
+          <SDKs />
+          <MultiTenancy />
+          <WhyItExists />
+          <ClosingPosition />
+        </main>
+        <Footer />
+      </div>
 
       <UseCasesPopup
         isOpen={isUseCasesOpen}
