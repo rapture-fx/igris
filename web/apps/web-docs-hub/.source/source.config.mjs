629cce492870a28fb6f1a9fb20999ab4650d5b6c
Wira Mahendra <wiramahendra@proton.me>
2026-04-09 22:14:57 +0800

    Update source.config.mjs

--- web/apps/web-docs-hub/.source/source.config.mjs
+++ web/apps/web-docs-hub/.source/source.config.mjs
@@ -1,9 +1,19 @@
 // source.config.ts
+import { remarkMdxFiles } from "fumadocs-core/mdx-plugins";
 import { defineDocs, defineConfig } from "fumadocs-mdx/config";
 var docs = defineDocs({
-  dir: "content/docs"
+  dir: "content/docs",
+  docs: {
+    postprocess: {
+      extractLinkReferences: true
+    }
+  }
 });
-var source_config_default = defineConfig();
+var source_config_default = defineConfig({
+  mdxOptions: {
+    remarkPlugins: [remarkMdxFiles]
+  }
+});
 export {
   source_config_default as default,
   docs
