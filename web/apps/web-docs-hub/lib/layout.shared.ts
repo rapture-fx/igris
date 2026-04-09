705b2b16b93bdded2dd1e545e48931cb22f11a5d
Wira Mahendra <wiramahendra@proton.me>
2026-04-09 22:06:00 +0800

    Create layout.shared.ts

---
+++ web/apps/web-docs-hub/lib/layout.shared.ts
@@ -0,0 +1,10 @@
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+
+export function baseOptions(): BaseLayoutProps {
+  return {
+    nav: {
+      title: 'Igris Docs',
+    },
+    githubUrl: 'https://github.com/wiramahendra/Schlep-engine',
+  };
+}
