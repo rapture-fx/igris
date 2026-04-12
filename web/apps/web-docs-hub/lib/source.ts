fc5e8d3102308e9ca7fe4acf1338d627c4b4f511
Wira Mahendra <wiramahendra@proton.me>
2026-04-12 13:49:53 +0800

    Update source.ts

--- web/apps/web-docs-hub/lib/source.ts
+++ web/apps/web-docs-hub/lib/source.ts
@@ -1,7 +1,10 @@
 import { docs } from '@/.source/server';
 import { loader } from 'fumadocs-core/source';
+import { createSidebarTree } from '@/lib/sidebar-tree';
 
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
 });
+
+export const sidebarTree = createSidebarTree(source.pageTree);
