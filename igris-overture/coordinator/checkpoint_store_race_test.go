1cc71e1ffe05c2ed3d53e83874979ad5cb98ee69
Wira Mahendra <wiramahendra@proton.me>
2026-04-16 23:41:13 +0800

    Update checkpoint_store_race_test.go

--- igris-overture/coordinator/checkpoint_store_race_test.go
+++ igris-overture/coordinator/checkpoint_store_race_test.go
@@ -345,6 +345,32 @@ func TestCheckpointStoreRejectsInconsistentCheckpointWatermark(t *testing.T) {
 	}
 }
 
+func TestCheckpointStoreRejectsIncompleteCheckpointWatermark(t *testing.T) {
+	t.Parallel()
+
+	db, queued := newQueuedExecDB(t)
+	store := NewCheckpointStore(db)
+	taskID := uuid.New()
+
+	err := store.SaveCheckpoint(&CheckpointPayload{
+		TaskID: taskID,
+		ResumeToken: ResumeToken{
+			LastCommittedStep: 5,
+			CheckpointDigest:  "digest-5",
+			RuntimeID:         "runtime-1",
+		},
+		WalEntries: []WalEntry{
+			{EntryID: uuid.New(), TaskID: taskID, StepIndex: 4},
+		},
+	})
+	if !errors.Is(err, ErrTaskTransitionRejected) {
+		t.Fatalf("SaveCheckpoint() error = %v, want ErrTaskTransitionRejected", err)
+	}
+	if queued.remainingExecs() != 0 {
+		t.Fatalf("remaining execs = %d, want 0", queued.remainingExecs())
+	}
+}
+
 func TestCheckpointStoreRejectsForeignTaskWalEntry(t *testing.T) {
 	t.Parallel()
 
