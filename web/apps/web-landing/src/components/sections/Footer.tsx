8fef3cb9ee7ef9efb31a5cd0242eb9a0e1a48d13
Wira Mahendra <wiramahendra@proton.me>
2026-05-19 22:00:24 +0800

    Update Footer.tsx

--- web/apps/web-landing/src/components/sections/Footer.tsx
+++ web/apps/web-landing/src/components/sections/Footer.tsx
@@ -79,7 +79,7 @@ export default function Footer() {
 
   return (
     <footer className="bg-white dark:bg-dark-bg text-gray-900 dark:text-[#f6f6f4] transition-colors duration-200">
-      <div className="mx-auto max-w-[1320px] px-4 sm:px-6 lg:px-8">
+      <div className="mr-auto max-w-[1000px] px-4 sm:px-6 lg:px-8">
         <div className="px-2 md:px-4 lg:px-6 flex flex-col min-h-[480px]">
 
           {/* Link columns + logo */}
