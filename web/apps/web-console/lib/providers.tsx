d2beffcaed800d74a5de97f622c12b545157481c
Wira Mahendra <wiramahendra@proton.me>
2026-05-27 16:45:57 +0800

    Update providers.tsx

--- web/apps/web-console/lib/providers.tsx
+++ web/apps/web-console/lib/providers.tsx
@@ -2,7 +2,6 @@
 
 import * as React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from '@/components/providers/ThemeProvider';
 
 function makeQueryClient() {
@@ -42,9 +41,6 @@ export function Providers({ children }: { children: any }) {
     >
       <QueryClientProvider client={queryClient}>
         {children}
-        {process.env.NODE_ENV === 'development' && (
-          <ReactQueryDevtools initialIsOpen={false} position="bottom" />
-        )}
       </QueryClientProvider>
     </ThemeProvider>
   );
