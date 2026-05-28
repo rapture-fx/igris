f27359a2122ef978f9e649de1f4d53ad965033d5
Wira Mahendra <wiramahendra@proton.me>
2026-05-29 07:44:06 +0800

    Update home_controller.rb

--- web/apps/rails-console/app/controllers/home_controller.rb
+++ web/apps/rails-console/app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @actions = data_source.actions
     @recent_runs = data_source.recent_runs(limit: 3)
+    @runtimes = data_source.runtimes
     @daily_runs = data_source.daily_run_counts
     @degraded_error = data_source.error
   end
